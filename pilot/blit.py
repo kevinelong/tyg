37b4a699569938a1eae73c19d5a01f593ea14eb0
kevinelong <kevinelong@gmail.com>
2021-01-23 08:36:46 -0800

    split tests

--- pilot/blit.py
+++ pilot/blit.py
@@ -1,31 +1,10 @@
 def blit(source,target,ox,oy):
-	for sy in range(len(source)):
-		for sx in range(len(source[0])):
-			target[sy+oy][sx+ox] = source[sy][sx]
+  for sy in range(len(source)):
+    for sx in range(len(source[0])):
+      target[sy+oy][sx+ox] = source[sy][sx]
 
 def blit_wrap(source,target,ox,oy):
-	for sy in range(len(source)):
-		for sx in range(len(source[0])):
-			target[(sy+oy) % len(target)][(sx+ox) % len(target[0])] = source[sy][sx]
-source = [
-	[1,0,0,1],
-	[0,1,1,0],
-	[0,1,1,0],
-	[1,0,0,1]
-]
-target = [
-	[0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0]
-]
-blit_wrap(source,target,5,5)
-for row in target:
-	for value in row:
-		print(value,end="")
-	print("")
+  for sy in range(len(source)):
+    for sx in range(len(source[0])):
+      target[(sy+oy) % len(target)][(sx+ox) % len(target[0])] = source[sy][sx]
 
